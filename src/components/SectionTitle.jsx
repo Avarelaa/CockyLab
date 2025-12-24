dca30c21a0e7435ab02d54ad0b14936e28f24410
Avarelaa <alfredo.varela.armendariz@gmail.com>
2025-12-24 16:20:16 -0300

    Delete src directory

--- src/components/SectionTitle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const SectionTitle = ({ children, align = "center" }) => (
-  <h2 className={`text-3xl md:text-4xl font-bold text-[#283a42] mb-6 text-${align} leading-tight`}>
-    {children}
-  </h2>
-);
